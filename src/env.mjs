7d39d1d7cc62c968341a8c7fcfbf3f5e501caa90
knhn1004 <kunhan945@gmail.com>
2023-04-23 11:32:35 -0700

    husky pre-commit hook
    ptr

--- src/env.mjs
+++ src/env.mjs
@@ -28,7 +28,7 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object(
-  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */(
+  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ (
     {
       // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
     }
